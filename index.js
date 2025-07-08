1f0e9eedc59b4a751f590e36069e5ce7931aeed2
Rakesh-Git123 <rakeshgit1234@gmail.com>
2025-07-08 11:18:52 +0530

    updated

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 import mongoose from "mongoose";
 import express from "express";
-import userRouter from "./routes/userRoute.js"
-import dotenv from "dotenv"
-dotenv.config()
+import dotenv from "dotenv";
+import userRouter from "./routes/userRoute.js";
+dotenv.config();
 
 const app = express();
+const dbuser = encodeURIComponent(process.env.DBUSERNAME);
+const dbpass = encodeURIComponent(process.env.DBPASSWORD);
 
-mongoose.connect(`mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@project1.t8s8jn0.mongodb.net/merndb?retryWrites=true&w=majority&appName=Project1`).then(() => {
-  app.listen(8080, () => {
-    console.log("Server started");
+// mongoose
+//   .connect(`mongodb://${dbuser}:${dbpass}@localhost:27017/lpu?authsource=admin`)
+//   .then(() => {
+//     app.listen(8080, () => {
+//       console.log("Server started");
+//     });
+//   });
+
+  mongoose
+  .connect(`mongodb+srv://${dbuser}:${dbpass}@project1.t8s8jn0.mongodb.net/merndb?retryWrites=true&w=majority&appName=Project1`)
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server started");
+    });
   });
-});
 
 app.use(express.json());
 
